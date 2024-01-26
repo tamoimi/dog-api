f4c26ad490d0c98d7d05b579f3f2ba7627999288
tami <tamiortami@gmail.com>
2024-01-26 17:37:04 +0900

    feat: spell-api

--- app/api/harry-potter.ts
+++ app/api/harry-potter.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
-const baseUrl = "https://hp-api.onrender.com/api/characters";
+const baseUrl = "https://hp-api.onrender.com/api";
 
-// harry-potter api
+// harry-potter character api
 export const getCharacters = () => {
-  return axios.get(`${baseUrl}`).then((res) => res.data);
+  return axios.get(`${baseUrl}/characters`).then((res) => res.data);
+};
+
+// harry-potter spell api
+export const getSpells = () => {
+  return axios.get(`${baseUrl}/spells`).then((res) => res.data);
 };
