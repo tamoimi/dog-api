5a58053c98368b650bfc42ba5ccd23383366544f
tami <tamiortami@gmail.com>
2024-01-26 16:30:22 +0900

    style: card style

--- app/components/CharacterCard.tsx
+++ app/components/CharacterCard.tsx
@@ -18,14 +18,49 @@ const CharacterCard = () => {
         <Loading />
       ) : (
         <>
-          <div className="grid md:grid-cols-2 sm:grid-cols-1">
-            {data?.map((p: any) => (
-              <div key={p.id} className="m-5">
-                <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{p.name}</h5>
-                  <p className="text-gray-700 dark:text-gray-400">{p.house}</p>
-
-                  <Image src={p.image} alt={p.image} width={200} height={200} />
+          <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-6">
+            {data?.slice(0, 20).map((p: any) => (
+              <div
+                className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+                key={p.id}
+              >
+                <a href="#">
+                  <Image
+                    className="rounded-t-lg"
+                    src={p.image}
+                    alt={p.image}
+                    width={0}
+                    height={0}
+                    style={{ width: "100%", height: "auto" }}
+                    sizes="100vw"
+                  />
+                </a>
+                <div className="p-5">
+                  <a href="#">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{p.name}</h5>
+                  </a>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{p.home}</p>
+                  <a
+                    href="#"
+                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Read more
+                    <svg
+                      className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 14 10"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M1 5h12m0 0L9 1m4 4L9 9"
+                      />
+                    </svg>
+                  </a>
                 </div>
               </div>
             ))}
