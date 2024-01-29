dd20c3b3819489dd9d70d1f5e2854270ffebd132
tami <tamiortami@gmail.com>
2024-01-29 17:58:06 +0900

    feat: link

--- app/components/CharacterCard.tsx
+++ app/components/CharacterCard.tsx
@@ -2,15 +2,15 @@ import useSWR from "swr";
 import { getCharacters } from "../api/harry-potter";
 import Loading from "../loading";
 import Image from "next/image";
+import Link from "next/link";
 
 const CharacterCard = () => {
+  // swr --------------------------------------------------------------------------------------- swr
   const { data, isLoading } = useSWR("api/characters", getCharacters, {
     refreshInterval: 86400000, // refresh once a day
     revalidateOnFocus: false,
     revalidateOnReconnect: false,
   });
-
-  console.log("data", data);
 
   return (
     <>
@@ -24,7 +24,7 @@ const CharacterCard = () => {
                 className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
                 key={p.id}
               >
-                <a href="#">
+                <Link href={`/character/${p.id}`}>
                   <Image
                     className="rounded-t-lg"
                     src={p.image}
@@ -34,21 +34,21 @@ const CharacterCard = () => {
                     style={{ width: "100%", height: "auto" }}
                     sizes="100vw"
                   />
-                </a>
+                </Link>
                 <div className="p-5">
-                  <a href="#">
+                <Link href={`/character/${p.id}`}>
                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                       {p.name}
                     </h5>
-                  </a>
+                  </Link>
                   <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                     {p.dateOfBirth}
                   </p>
                   <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{p.house}</p>
                   <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{p.home}</p>
-                  <a
-                    href="#"
-                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-yellow-900 rounded-lg hover:bg-yellow-800  dark:bg-blue-600 dark:hover:bg-yello-700 dark:focus:ring-yellow-800"
+                  <Link
+                    href={`/character/${p.id}`}
+                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-yellow-900 rounded-lg hover:bg-yellow-800 "
                   >
                     Read more
                     <svg
@@ -66,7 +66,7 @@ const CharacterCard = () => {
                         d="M1 5h12m0 0L9 1m4 4L9 9"
                       />
                     </svg>
-                  </a>
+                  </Link>
                 </div>
               </div>
             ))}
