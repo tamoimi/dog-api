e0eaaf0b20964ec95a7474645a838f52a9f8bee4
tami <tamiortami@gmail.com>
2024-03-05 14:19:49 +0900

    feat: new font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,19 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Montserrat } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Be Happy!",
-  description: "The dog API by Tami",
+  title: "Everything about Harry Potter!",
+  description: "The Harry Potter API by Tami",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-        <body className={inter.className}>{children}</body>
+      <body className={montserrat.className}>{children}</body>
     </html>
   );
 }
