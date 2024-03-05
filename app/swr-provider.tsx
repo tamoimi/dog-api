e2df28245b481f86894aeff777eb53529f6cb297
tami <tamiortami@gmail.com>
2024-03-05 14:19:38 +0900

    remove: files

--- app/swr-provider.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { SWRConfig } from "swr";
-
-export const SWRProvider = ({ children }: any) => {
-  return <SWRConfig value={{ provider: () => new Map() }}>{children}</SWRConfig>;
-};
