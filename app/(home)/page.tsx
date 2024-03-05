c6b50c6cc92bfc404e8bf1f46ed58ee32f9b5f47
tami <tamiortami@gmail.com>
2024-03-05 14:20:20 +0900

    feat: home page

---
+++ app/(home)/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Cards from "../components/CharacterCard";
+import SpellCard from "../components/SpellCard";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-8 gap-6">
+      <p className="mb-4 text-2xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        Everything about&nbsp;
+        <span className="underline underline-offset-3 decoration-8 decoration-red-800 dark:decoration-red-800">
+          Harry Potter
+        </span>
+      </p>
+      <SpellCard />
+      <Cards />
+    </main>
+  );
+}
