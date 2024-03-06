4c3707f66b1ee0891e1b3888154124ad3c47786a
tami <tamiortami@gmail.com>
2024-03-06 16:01:19 +0900

    style

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,7 +5,7 @@ import SpellCard from "../components/SpellCard";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8 gap-6">
-      <p className="mb-4 text-2xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+      <p className="mb-4 text-xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
         Everything about&nbsp;
         <span className="underline underline-offset-3 decoration-8 decoration-red-800 dark:decoration-red-800">
           Harry Potter
