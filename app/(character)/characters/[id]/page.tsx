37a0f9596c6f3ed11c222c72940ac4efd0571bf2
tami <tamiortami@gmail.com>
2024-03-07 10:52:43 +0900

    feat: loading component

--- app/(character)/characters/[id]/page.tsx
+++ app/(character)/characters/[id]/page.tsx
@@ -1,9 +1,10 @@
 import DetailCard from "@/app/components/DetailCard";
+import Loading from "@/app/components/Loading";
 import { Suspense } from "react";
 
 export default async function CharacterDetail({ params: { id } }: { params: { id: string } }) {
   return (
-    <Suspense fallback={<>loading...</>}>
+    <Suspense fallback={<Loading />}>
       <DetailCard id={id} />
     </Suspense>
   );
