dd111e4324fba7a0aba73d0bd9735e9715732ae5
tami <tamiortami@gmail.com>
2024-03-25 11:29:02 +0900

    style: character card

--- app/components/CharacterCard.tsx
+++ app/components/CharacterCard.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { API_URL } from "../api/harry-potter";
+import { HiArrowSmallRight } from "react-icons/hi2";
 
 async function getCharacter() {
   const response = await fetch(`${API_URL}/characters`);
@@ -31,37 +32,21 @@ export default async function CharacterCard() {
               />
             </Link>
             <div className="p-2">
-              <Link href={"/"}>
-                <h5 className="mb-2 text-sm font-bold tracking-tight text-gray-900 dark:text-white">
-                  {p.name}
-                </h5>
-              </Link>
-              <p className=" text-gray-700 dark:text-gray-400 font-medium">
-                House: <span>{p.house}</span>
-              </p>
-              <p className=" font-medium text-gray-700 dark:text-gray-400 mb-2">
-                Gender: {p.gender}
-              </p>
+              <h5 className="mb-2 font-bold tracking-tight text-gray-900 dark:text-white">
+                {p.name}
+              </h5>
+
+              <div className="flex gap-1">
+                <span className=" text-neutral-700 font-semibold">House:</span>
+                <span>{p.house}</span>
+              </div>
+
               <Link
                 href={`/characters/${p.id}`}
-                className="inline-flex items-center p-2 text-sm text-center text-white bg-yellow-900 rounded-lg hover:bg-yellow-800 "
+                className="inline-flex items-center font-semibold p-2 text-xs text-center bg-yellow-600 rounded-lg hover:bg-yellow-700 transition duration-300 mt-3"
               >
                 Read more
-                <svg
-                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                  />
-                </svg>
+                <HiArrowSmallRight className="ml-1" />
               </Link>
             </div>
           </div>
