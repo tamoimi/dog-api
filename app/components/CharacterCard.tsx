2c066d9d0de25e956a569d33da5b5c1c3813408b
tami <tamiortami@gmail.com>
2024-03-25 11:59:35 +0900

    error: icon

--- app/components/CharacterCard.tsx
+++ app/components/CharacterCard.tsx
@@ -46,7 +46,22 @@ export default async function CharacterCard() {
                 className="inline-flex items-center font-semibold p-2 text-xs text-center bg-yellow-600 rounded-lg hover:bg-yellow-700 transition duration-300 mt-3"
               >
                 Read more
-                <HiArrowSmallRight className="ml-1" />
+                {/* <HiArrowSmallRight className="ml-1" /> */}
+                <svg
+                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
               </Link>
             </div>
           </div>
