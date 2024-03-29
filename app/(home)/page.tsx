a64da5b670b076ec7ceac6918d28a80c00563ec8
tami <tamiortami@gmail.com>
2024-03-29 14:57:38 +0900

    sytle: padding style

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,7 +4,7 @@ import SpellCard from "../components/SpellCard";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-8 gap-6">
+    <main className="flex min-h-screen flex-col items-center justify-between p-8 gap-6 md:px-60">
       <p className="mb-4 text-xl font-bold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
         Everything about&nbsp;
         <span className="underline underline-offset-3 decoration-8 decoration-red-800 dark:decoration-red-800">
